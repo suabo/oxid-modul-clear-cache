603e60ac50fe305bdcede34daaa1324ba7934b43
suabo <info@suabo.de>
2014-04-23 22:07:55 +0200

    Fix für manche OXID Versionen in metadata.php Ordner für Beschreibung hinzugefügt, Inhalte werden nicht getracked Ordner für Tags eingefügt, Tags werden nicht getracked

--- copy_this/modules/suabo/clearcache/metadata.php
+++ copy_this/modules/suabo/clearcache/metadata.php
@@ -15,10 +15,11 @@ $aModule = array(
       'en' => 'Clear Shopcache in admin backend.'
     ),
     'thumbnail'   => '../logo.png',
-    'version'     => '1.1.5',
+    'version'     => '1.1.6',
     'author'      => 'suabo',
     'url'         => 'http://www.suabo.de',
     'email'       => 'info@suabo.de',
+    'extend' => array(),
     'files' => array(
         'mgclearcache' => 'suabo/clearcache/controllers/admin/mgclearcache.php',
     ),
